f03809706a96b91e3cb62f5960bc8950753b83b8
erinelise2 <erinelise2@gmail.com>
2023-07-17 11:53:31 -0700

    commiting boiler plate from lab

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
 import React from 'react';
 import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
+
+import Home from './Components/Home.js';
+import About from './Components/About.js';
+import Observations from './Components/Observations.js'
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello World 🌎</h1>
-    </div>
+
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/observations">Observations</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/observations">
+            <Projects/>
+          </Route>
+
+					<Route path="/about">
+            <About />
+          </Route>
+           
+					<Route path="/">
+            <Home />
+          </Route>
+          
+				</Switch>
+      </div>
+    </Router>
   );
 }
---
+++ src/Components/Observations.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Observations() {
+  return (
+    <div>Observations</div>
+  )
+}
