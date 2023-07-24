3b717ca870937ba46f2ab5248ac7c979a8000c75
erinelise2 <erinelise2@gmail.com>
2023-07-24 09:37:18 -0700

    added ideas from Natalie's class

--- src/App.js
+++ src/App.js
@@ -1,31 +1,57 @@
 import React from 'react';
 import './App.css';
 
+import { 
+  Container,
+  Nav,
+  Navbar
+} from "react-bootstrap";
+
 import {
   BrowserRouter as Router,
   Route,
   Routes,
+  NavLink,
+  Link
 } from 'react-router-dom';
 
 import Home from './Pages/Home';
 import About from './Pages/About';
 import Observations from './Pages/Observations';
-import Navigation from './Components/Navigation';
+import data from './Components/FieldGuide'
+
 
 export default function App() {
-  return (
+  const [fieldGuide, setFieldGuide] = useState(data)
+  
+  // const addObservation = {observationID} => {
+    // const newObservation = {
+      // id. and other identifiers
+      // 
+  //   }
+  // }
 
+  // ** add: addObservation={addObservation} to home element path below after field guide
+
+  return (
     <Router>
-      <Navigation />
-      <div>
-        <main>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+        <Navbar.Brand as={Link} to="/">Your Field Guide</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+          <Nav.Link as={NavLink} to="/about">About</Nav.Link>
+          <Nav.Link as={NavLink} to="/observations">Observations</Nav.Link>
+        </Nav>
+        </Container>
+      </Navbar>
+      <Container>
         <Routes>
-          <Route path="/observations" element={<Observations/>}/>
+          <Route path="/observations" element={<Observations fieldGuide={fieldGuide} />}/>
           <Route path="/about" element={<About/>}/>
-          <Route path="/" element={<Home/>}/>  
+          <Route path="/" element={<Home fieldGuide={fieldGuide}  />}/>  
 				</Routes>
-        </main>
-      </div>
+      </Container>
     </Router>
   );
   // import the new observation form to link to the observation page where there will be a field guide table connected to the update observation form?
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
-export default function Home() {
-		return <h2>Home</h2>;	
+export default function Home({ fieldGuide, addObservation }) {
+		return (
+		<div>
+			<h2>Home</h2>;
+			{fieldGuide.map(observation => (
+				<div className="border shadow p-4 m-4">
+					{/* build table based on observation data props/components */}
+				</div>
+			))}
+		</div>	
+		)
 }
